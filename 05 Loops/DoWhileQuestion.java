9d3aa4e7e577cb91b2c32705f3e3baf81a5cd2d9
AyUsHi134 <singhayushi9369@gmail.com>
2024-05-15 23:51:55 +0530

    Loops

---
+++ 05 Loops/DoWhileQuestion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DoWhileQuestion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = -1;
+
+        do{  
+            if(number != -1) {
+          System.out.println("Cube is " + (number * number * number));
+            }
+          System.out.println("Enter a number:");
+         number = sc.nextInt();
+        } while(number >= 0);
+        System.out.println("Thank You! Have Fun!");
+
+    }
+}
