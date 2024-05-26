6f79a393de67720e5dbcb215b721790255924c3b
AyUsHi134 <singhayushi9369@gmail.com>
2024-05-26 23:57:35 +0530

    Interface

---
+++ 02 OOPs/InnerOperation.java
@@ -0,0 +1,41 @@
+interface Operation {
+    int perform(int x, int y);
+}
+
+class Add implements Operation {
+        @Override
+        public int perform(int x,int y) {
+            return x + y;
+
+        }
+}
+
+class Subtract implements Operation{
+
+    @Override
+    public int perform(int x, int y) {
+        return x - y;
+    }
+
+
+}
+
+class Divide implements Operation {
+
+    @Override
+    public int perform(int x, int y) {
+        if(y == 0){
+            return -1;
+        }
+        return x / y;
+    }
+
+}
+    
+public class InnerOperation {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
