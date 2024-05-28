e486891e277c7df1ec3abd9513f82f0484d1d1b0
AyUsHi134 <singhayushi9369@gmail.com>
2024-05-28 23:47:28 +0530

    Collections

---
+++ 08 Collections/Anagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Anagram {
+    
+    public boolean areAnagrams(String str1, String str2){
+        if (str1 == null || str2 == null) {
+            return false;
+        }
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        String lowercaseStr1 = str1.toLowerCase();
+        String lowercaseStr2 = str2.toLowerCase();
+
+        char[] charArray1 = lowercaseStr1.toCharArray();
+        char[] charArray2 = lowercaseStr2.toCharArray();
+
+        Arrays.sort(charArray1); 
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+
+ }
+}
