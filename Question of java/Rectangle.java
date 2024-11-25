473123e58b62b5ce7a656058bcf6d1d6fe371ee9
Ayushi Singh <singhayushi9369@gmail.com>
2024-11-26 01:48:13 +0530

    OOPs

---
+++ Question of java/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+    public int length;
+    public int breadth;
+  
+    public Rectangle(int length, int breadth){// constructor
+     System.out.println("I am inside constructor");
+     this.length = length; // calling instance variable 
+     this.breadth = breadth;
+    }
+  public void draw(){
+    System.out.println("Drawing a rectangle");
+  
+
+  }
+
+  public void Area(){
+    System.out.println("Area is: "+ (length*breadth ));
+  }
+}
