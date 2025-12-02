36c5137bd2be975f65605719721eb1f0d34ea926
Ayushi Singh <singhayushi9369@gmail.com>
2025-12-03 00:37:51 +0530

    java Intro

---
+++ Java-Revision/1.Introduction/FiveLines.java
@@ -0,0 +1,11 @@
+public class FiveLines {
+    public static void main(String[]args){
+        System.out.println("hi");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("Ayushi");
+
+    }
+    
+}
---
+++ Java-Revision/1.Introduction/WithoutStatic.java
@@ -0,0 +1,7 @@
+public class WithoutStatic {
+    public void main(String[] args) {
+        System.out.println("Ayushi Singh");
+    }   
+}
+
+// This file does not execute because main() is not static, so JVM cannot call it.
