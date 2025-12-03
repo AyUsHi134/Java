28ce1337e8ad10c076ffddc8448334eace5d32aa
Ayushi Singh <singhayushi9369@gmail.com>
2025-12-04 01:22:25 +0530

    Variables, TypeCasting, Operators

---
+++ Java_Revision/src/Variables/OperatorsDemo.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int a = 5; 
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(--a);
+        String b = (a > 4) ? "false": "true";
+        System.out.println("value of b is " + b);
+
+        
+    }
+}
---
+++ Java_Revision/src/Variables/TypeCasting.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        int number1 = 3;
+        double number2 = number1;
+        int number3 = (int) number2;
+        System.out.println("number one is " + number1);
+        System.out.println("number 2 is " + number2);
+        System.out.println("number 3 is " + number3);
+
+    }
+}
